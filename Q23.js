5cc4c8911e7959bfafaaacbaa95a2d759bf2a2e8
Muhammad Naeem Warsi <161728783+MuhammadNaeemWarsi@users.noreply.github.com>
2024-04-01 00:44:18 +0500

    Assignment 23 complete

---
+++ Q23.js
@@ -0,0 +1,19 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log(car == 'civic');
+var num = 20;
+console.log(num == 20);
+console.log(num == 15);
+var cName = 'Naeem Warsi';
+console.log(cName == 'Naeem Warsi');
+console.log(cName == 'Ahemd');
+var fruitName = 'Mango';
+console.log(fruitName == 'Mango');
+console.log(fruitName == 'Banana');
+var wName = "Hello world";
+console.log(wName == "Hello world");
+console.log(wName == "New world");
